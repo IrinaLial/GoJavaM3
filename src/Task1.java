c408cf1baf7587ee5b10586283b5e776c874f2c3
Iryna Lialenko <lyalenko.iryna@gmail.com>
2017-09-18 23:47:29 +0300

    Adjust code style settings

--- src/Task1.java
+++ src/Task1.java
@@ -1,42 +1,42 @@
 import java.util.Scanner;
 
 public class Task1 {
-    public static void main(String[]args){
-        Scanner in = new Scanner(System.in);
-        System.out.println("Type array size:");
+    public static void main(String[] args) {
+        Scanner in = new Scanner ( System.in );
+        System.out.println ( "Type array size:" );
         int[] nums;
-        nums = new int[in.nextInt()];
+        nums = new int[in.nextInt ()];
 
-        numsFilling(nums, in);
-        findMinvalue(nums);
-        findMaxvalue(nums);
-        findRepetitions(nums, 5);
-        sortingNums(nums);
+        numsFilling ( nums, in );
+        findMinvalue ( nums );
+        findMaxvalue ( nums );
+        findRepetitions ( nums, 5 );
+        sortingNums ( nums );
     }
 
     public static void numsFilling(int[] nums, Scanner in) {
         for (int i = 0; i < nums.length; i++) {
-            System.out.println("Type " + (i + 1) + " index:");
-            nums[i] = in.nextInt();
+            System.out.println ( "Type " + (i + 1) + " index:" );
+            nums[i] = in.nextInt ();
         }
     }
 
     public static void findMinvalue(int[] nums) {
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < nums.length; i++) {
-            min = Math.min(min, nums[i]);
+            min = Math.min ( min, nums[i] );
         }
 
-        System.out.println("Minimum value of Nums: " + min);
+        System.out.println ( "Minimum value of Nums: " + min );
     }
 
     public static void findMaxvalue(int[] nums) {
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < nums.length; i++) {
-            max = Math.max(max, nums[i]);
+            max = Math.max ( max, nums[i] );
         }
 
-        System.out.println("Max value of Nums: " + max);
+        System.out.println ( "Max value of Nums: " + max );
 
     }
 
@@ -47,13 +47,12 @@ public class Task1 {
                 r++;
             }
         }
-
-        System.out.println("Repetitions of " + n + "  = " + r);}
-
+        System.out.println ( "Repetitions of " + n + "  = " + r );
+    }
 
 
     public static void sortingNums(int[] nums) {
-        System.out.println("Sorted array: ");
+        System.out.println ( "Sorted array: " );
         for (int i = 0; i < nums.length; i++) {
             for (int j = 1; j < (nums.length - i); j++) {
                 if (nums[j - 1] > nums[j]) {
@@ -64,7 +63,7 @@ public class Task1 {
             }
         }
 
-        for(int i = 0; i < nums.length; i++)
-            System.out.println("nums[" + i + "] = " + nums[i]);
+        for (int i = 0; i < nums.length; i++)
+            System.out.println ( "nums[" + i + "] = " + nums[i] );
     }
 }
