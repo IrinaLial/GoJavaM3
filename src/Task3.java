dfc88ac02513a65e5327af29181d324b59ad413f
Iryna Lialenko <lyalenko.iryna@gmail.com>
2017-09-18 17:03:41 +0300

    Add solution

--- src/Task3.java
+++ src/Task3.java
@@ -2,6 +2,23 @@ import java.util.Scanner;
 
 public class Task3 {
     public static void main (String[]args){
-        
+        Scanner scanner = new Scanner(System.in);
+
+        String line = scanner.nextLine();
+        String[] numbers = line.split(" ");
+
+        int[] realNumbers = new int[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            realNumbers[i] = Integer.parseInt(numbers[i]);
+
+        }
+
+        for (int realNumber : realNumbers) {
+            System.out.println(realNumber);
+
+        }
+        System.out.println();
+        System.out.println("Your array size is :" + realNumbers.length);
+
     }
 }
