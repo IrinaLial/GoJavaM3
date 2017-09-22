3ba8b82ddb74e52587ce334cd857b384c39730f6
Iryna Lialenko <lyalenko.iryna@gmail.com>
2017-09-23 01:08:05 +0300

    add exception

--- src/Task3.java
+++ src/Task3.java
@@ -1,24 +1,25 @@
 import java.util.Scanner;
 
 public class Task3 {
-    public static void main (String[]args){
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner ( System.in );
 
-        String line = scanner.nextLine();
-        String[] numbers = line.split(",");
+        String line = scanner.nextLine ();
+        String[] numbers = line.split ( " " );
 
         int[] realNumbers = new int[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
-            realNumbers[i] = Integer.parseInt(numbers[i]);
+            realNumbers[i] = Integer.parseInt ( numbers[i] );
+            try {
+                realNumbers[i] = Integer.parseInt ( numbers[i] );
+            } catch (Exception e) {
+                System.out.println(numbers[i] + " Not int number!!!");
+            }
 
-        }
-
-        for (int realNumber : realNumbers) {
-            System.out.println(realNumber);
-
-        }
-        System.out.println();
-        System.out.println("Your array size is :" + realNumbers.length);
-
+            for (int realNumber : realNumbers) {
+                System.out.println ( realNumber );
+            }
+            System.out.println ();
+        System.out.println ( "Your array size is :" + realNumbers.length );
     }
-}
+}}
